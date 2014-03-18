9484ae6e0254705704780bd8062763a61cc42bb1
Artur Jamro (Mrowqa) <artur.jamro@gmail.com>
2014-03-18 15:13:24 +0100

    Continued working on primes generator (sending cache to slaves by master)

--- code/primes_gen.cpp
+++ code/primes_gen.cpp
@@ -2,8 +2,9 @@
 #include <vector>
 #include <mpi.h>
 #include <string>
-#include <list>
+#include <deque>
 #include <fstream>
+#include <map>
 using namespace std;
 
 void doMaster();
@@ -22,20 +23,25 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
-int loadCache(list<vector<int>>& primes, int blockSize, string cacheLocation) {
-	fstream readStr;
+#define PRIMES_SEGMENT_SIZE_TAG		1
+#define PRIMES_SEGMENT_DATA_TAG		2
+#define QUERY_TAG					3
+
+// ---------------------------------------- MASTER ---------------------------
+int loadCache(deque<vector<long long>>& primes, long long blockSize, string cacheLocation) {
+	ifstream readStr;
 	int loadedBlocks = 0;
 	ostringstream filename;
 	
 	primes.clear();
 	while(true) {
 		filename.str("");
-		filename << cacheLocation << "/primes-" << (loadedBlocks*blockSize+1) <<
+		filename << cacheLocation << "primes-" << (loadedBlocks*blockSize+1) <<
 			"-" << (loadedBlocks*(blockSize+1)) << ".txt";
 		readStr.open(filename.str(), ios::read);
 		if(readStr.open()) {
 			primes.push_back(vector<int>());
-			int primeNumber;
+			long long primeNumber;
 			while(readStr >> primeNumber)
 				primes.back().push_back(primeNumber);
 				
@@ -49,7 +55,17 @@ int loadCache(list<vector<int>>& primes, int blockSize, string cacheLocation) {
 	return loadedBlocks * blockSize;
 }
 
-bool saveCacheSegment(const vector<int>& primes, int from, int blockSize, string cacheLocation) {
+bool saveCacheSegment(const vector<long long>& primes, long long from, long long blockSize, string cacheLocation) {
+	ostringstream filename;
+	filename << cacheLocation << "primes-" << from << "-" << to << ".txt";
+	ofstream writeStr(filename.str(), io::trunc);
+	if(writeStr.open()) {
+		for(int i=0; i< primes.size(); i++)
+			writeStr << primes[i] << " ";
+		writeStr.close();
+	}
+	else
+		return false;
 	
 	return true;
 }
@@ -58,7 +74,7 @@ void doMaster() {
 	int procCnt = MPI::COMM_WORLD.Get_size();
 	cout << "Available " << procCnt << " processors.\n";
 	
-	int limit, blockSize;
+	long long limit, blockSize;
 	string cacheLocation;
 	cout << "Type searching upper bound: ";
 	cin >> limit;
@@ -69,22 +85,38 @@ void doMaster() {
 		return;
 	}
 	cout << "Type cache location (empty line for no cache): ";
-	cin.clear(); // Clear '\n' at end of the stdin stream
+	cin.sync(); // Clear '\n' at end of the stdin stream
 	getline(cin, cacheLocation);
+	if(cacheLocation[cacheLocation.size()-1] != '/')
+		cacheLocation += '/';
 	
-	list<vector<int>> primes;
+	deque<vector<long long>> primes;
 	if(!cacheLocation.empty()) {
-		int cachedTo = loadCache(&primes, blockSize, cacheLocation);
+		int cachedTo = loadCache(primes, blockSize, cacheLocation);
 		cout << "Loaded cached primes to " << cachedTo << "!\n";
+		
+		int loadedBlocks = primes.size();
+		for(int j=0; j<loadedBlocks; j++) {
+			for(int i=1; i<procCnt; i++) {
+				int segmentSize = primes[j].size();
+				MPI::COMM_WORLD.Isend(&segmentSize, 1, MPI::INT, i, PRIMES_SEGMENT_SIZE_TAG);
+				MPI::COMM_WORLD.Isend(&primes[j], &loadedBlocks, MPI::LONG_LONG, i, PRIMES_SEGMENT_DATA_TAG);
+			}
+		}
 	}
 	else
 		cout << "Computing without cache!\n";
 	
-
+	map<int,vector<long long>*> awaitingBlocks; // something else than pointer...
+	int nextBlockId = primes.size();
+	for(int i=primes.size(); i*blockSize <= limit; i++) {
+		
+	}
 	
 	cout << "Exiting master function.\n";
 }
 
+// ---------------------------------------- SLAVE ----------------------------
 void doSlave() {
 	int masterNode = 0;
 	
